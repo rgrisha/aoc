563567a1266e309d44316a37fba9cafc5b5ae178
Rolandas G <rol.griskevicius@gmail.com>
2022-10-20 08:24:00 +0300

    2021 day 14.1 refactor

--- 2021/src/aoc/day14.clj
+++ 2021/src/aoc/day14.clj
@@ -5,38 +5,34 @@
 
 
 (defn get-data []
-  (let [data  (u/get-day-data 14 #(s/split %1 #" -> ") )
+  (let [data  (u/get-day-data 14 #(s/split %1 #" -> "))
         chain (ffirst data)
-        data (drop 2 data)
-        ]
+        data (drop 2 data)]
+        
    [(seq chain) (into {} (map (fn [[a b]] [(seq a) (first b)]) data))]))
 
-(defn scan [instr chain]
-  (let [r   (map (fn [i c1 c2] [0 i (instr (list c1 c2))]) (drop 1 (range)) chain (next chain))
-        mrg (sort (fn [[p1 n1 _] [p2 n2 _]] (if (= n1 n2) (< p1 p2) (< n1 n2)) ) 
-                  (concat 
-                         r
-                         (map (fn [a b] [1 a b]) (range) chain)))
-        ]
-    (map #(nth %1 2) mrg)))
 
-(defn scan-n [instr -chain -n]
-  (loop [chain -chain n -n]
-    (if (zero? n)
-      chain
-      (recur (scan instr chain) (dec n)))
-
-    )
-  )
-
+(defn chain-replace [chain instr]
+  (map (fn [cc nc] (if-let [repl (get instr [cc nc])] [cc repl] [cc])) chain (next chain)))
+  
+(defn chain-replace [instr chain]
+  (first
+    (reduce (fn [[a n] v] (if-let [r (get instr [v (first n)])]
+                            [(conj (conj a v) r) (next n)] 
+                            [(conj a v)          (next n)]))
+            [[] (next chain)] 
+            chain))) 
+     
+    
 (defn run-1 []
   (let [[chain instr] (get-data)
-        chain-done (scan-n instr chain 40)
-        frqs  (vals  (frequencies chain-done))
-        frqss (sort frqs) 
-        
-        ]
-    frqss
-    (- (last frqss) (first frqss))
-    )
-  )
+        instr (into {} (map (fn [[[c1 c2] v]] [[c1 c2] v]) instr)) 
+        ;_ (println "c i " chain "---" instr "----" (chain-replace instr chain))      
+        chain (nth (iterate (partial chain-replace instr) chain) 10)
+        get-diff (fn [l] (- (second (last l)) (second (first l))))]
+    (->> chain      
+         frequencies 
+         (sort-by second) 
+         get-diff)))
+    
+    
