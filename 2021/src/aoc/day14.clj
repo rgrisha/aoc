ccb8ff8d4d3f5022d4d5c734dddc43f33ab6f754
Rolandas G <rol.griskevicius@gmail.com>
2022-10-25 22:55:52 +0300

    day 14 fucking done

--- 2021/src/aoc/day14.clj
+++ 2021/src/aoc/day14.clj
@@ -11,9 +11,6 @@
         
    [(seq chain) (into {} (map (fn [[a b]] [(seq a) (first b)]) data))]))
 
-
-(defn chain-replace [chain instr]
-  (map (fn [cc nc] (if-let [repl (get instr [cc nc])] [cc repl] [cc])) chain (next chain)))
   
 (defn chain-replace [instr chain]
   (first
@@ -29,10 +26,40 @@
         instr (into {} (map (fn [[[c1 c2] v]] [[c1 c2] v]) instr)) 
         ;_ (println "c i " chain "---" instr "----" (chain-replace instr chain))      
         chain (nth (iterate (partial chain-replace instr) chain) 10)
-        get-diff (fn [l] (- (second (last l)) (second (first l))))]
+        get-diff (fn [l] (- (second (last l)) (second (first l))))
+        _ (println "length of chain: " (count chain))]
     (->> chain      
          frequencies 
          (sort-by second) 
          get-diff)))
-    
-    
+
+(defn next-pair-gen [instr a [p1 p2] cnt]
+  (if-let [v (get instr [p1 p2])]
+    (-> a
+      (update [p1 v] (fn [ov] (+ (or ov 0) cnt)))
+      (update [v p2] (fn [ov] (+ (or ov 0) cnt)))) 
+    a))
+  
+
+(defn counted-iteration [instr pair-counts]
+  (reduce (fn [a [k v]] (next-pair-gen instr a k v)) {} pair-counts))
+
+(defn +? [v] (fn [ov] (+ (or ov 0) v)))
+
+(defn run-2 []
+  (let [[chain instr] (get-data)
+        instr (into {} (map (fn [[[c1 c2] v]] [[c1 c2] v]) instr)) 
+        ;_ (println "c i " chain "---" instr "----" (chain-replace instr chain))      
+        repl-fn (partial chain-replace instr)
+        chain1 (repl-fn chain) 
+        chain2 (repl-fn chain1)  
+        pair-counts (frequencies (map vector chain (rest chain)))
+        it1 (counted-iteration instr pair-counts)
+        it2 (counted-iteration instr it1)
+
+        final-iteration (nth (iterate (partial counted-iteration instr) pair-counts) 40)
+        stats (reduce (fn [a [[c1 c2] n]] (-> a (update-in [c1 :1] (+? n)) (update-in [c2 :2] (+? n)))) {} final-iteration)
+        stats (map (fn [[c m]] [c (max (:1 m) (:2 m))] ) stats)
+        sorted (sort-by second stats)]
+    (- (second (last sorted)) (second (first sorted)))))
+
