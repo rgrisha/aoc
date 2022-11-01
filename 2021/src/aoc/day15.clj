ef7f23f3b5aa553b5770f20735f056f21187207d
Rolandas G <rol.griskevicius@gmail.com>
2022-11-02 00:40:09 +0200

    day 15 part 1 solved

--- 2021/src/aoc/day15.clj
+++ 2021/src/aoc/day15.clj
@@ -8,82 +8,110 @@
        seq
        (mapv #(Integer. (str %1)))))
 
-;getting data for Dijkstra algo test
-(defn get-data-for-dj-test []
-  (let [data (u/get-day-data 15 parse-line )
-        data (vec data)]
-    (into {}
-      (for [y (range (count data)) x (range (count (get data y)))]
-        (let [p1 (get-in data [(inc y) x])
-              p2 (get-in data [y (inc x)])]
-          (do
-          [[y x]
-           (cond-> {}
-             p1 (assoc [(inc y) x] p1)
-             p2 (assoc [y (inc x)] p2))]))))))
-
-(defn get-data-z []
-  (let [data (u/get-day-data 15 parse-line )
-          data (vec data)]
-    data)  )
-
 (defn get-data []
-  (let [data (u/get-day-data 15 parse-line )
+  (let [data (u/get-day-data 15 parse-line)
         data (vec data)]
     (->
       (into {}
         (for [y (range (count data)) x (range (count (get data y)))]
           [[y x] {:v (get-in data [y x])}]))
-      (update [0 0] #(assoc %1 :path []))) ))
+      (update [0 0] #(assoc %1 :path [[0 0]] :min-v 0)))))
 
-(defn update-point [p {:keys [v path]}]
-  (let [parent-path path
-        nv v]
-    (fn [{:keys [v path]}] 
-      {:v (+ v nv) :path (conj parent-path p)})))
+(defn show [t a]
+  (println t a)
+  a)
 
-(defn min-point-2-directions [data [y x]]
-  (let [p1 [(dec y) x ]
-        p2 [y (dec x)]
-        p1n (get data p1)
-        p2n (get data p2)]
-    ;(println [y x] p1 p2 p1n p2n)
-    (cond
-      (nil? p1n)             (update data [y x] (update-point [y x] p2n))
-      (nil? p2n)             (update data [y x] (update-point [y x] p1n))
-      (< (:v p2n) (:v p1n))  (update data [y x] (update-point [y x] p2n))
-      :else                  (update data [y x] (update-point [y x] p1n)))))
+(defn find-shortest-node [nodes visited]
+  (->> nodes
+       (filter (fn [[n {min-v :min-v}]]
+                 (and (not (contains? visited n)) 
+                      (not (nil? min-v)))))  
+       (sort (fn [[_ {a :min-v}] [_ {b :min-v}]]  (compare a b)))
+       (first)))
 
-(defn min-point [data [y x]]
-  (let [p1 [(dec y) x ]
-        p2 [y (dec x)]
-        p1n (get data p1)
-        p2n (get data p2)]
-    ;(println [y x] p1 p2 p1n p2n)
-    (cond
-      (nil? p1n)             (update data [y x] (update-point [y x] p2n))
-      (nil? p2n)             (update data [y x] (update-point [y x] p1n))
-      (< (:v p2n) (:v p1n))  (update data [y x] (update-point [y x] p2n))
-      :else                  (update data [y x] (update-point [y x] p1n)))))
+(defn plus0 [a v] (+ (or a 0) v))
 
-(defn get-new-layer [data layer]
-  (let [new-points (mapcat (fn [[y x]] [[(inc y) x] [y (inc x)]]) layer)
-        new-points (filter (fn [[y x]] (get data [y x])) new-points)
-        new-points (into #{} new-points)  
-        data (reduce min-point data new-points)]
-    [data new-points]))
+(defn update-node [nodes key-of-node-to-update [n {min-v :min-v path :path} :as cn] path-weight-fn]
+  (let [upd-fn (fn [{min-vu :min-v :as upd-node}]
+                 (let [path-weight (path-weight-fn nodes key-of-node-to-update cn) 
+                       path-weight-from-current-node (plus0 min-v path-weight)]
+                   (if (or (nil? min-vu) (< path-weight-from-current-node min-vu)) 
+                     (assoc upd-node :min-v path-weight-from-current-node 
+                                     :path (conj (or path []) key-of-node-to-update))
+                     upd-node)))] 
+    (update nodes key-of-node-to-update upd-fn)))
 
-(defn calc-min-path [data'']
-  (loop [data data'' layer [[0 0]]]
-    (let [[data' new-layer] (get-new-layer data layer)]
-      (if (empty? new-layer)
-        (do
-          (-
-            (:v (get data (first layer)))
-            (:v (get data [0 0]))))
-        (recur data' new-layer)))))
+
+(defn update-adjacent [current-node nodes visited neighbours-fn path-weight-fn]
+  (reduce 
+    (fn [nodes v] (update-node nodes v current-node path-weight-fn))
+    nodes
+    (neighbours-fn nodes visited current-node)))
+    
+
+(defn neighbours-fn-2 [nodes visited [[x y] _]]
+  (for [xn [(dec x) x (inc x)] 
+        yn [(dec y) y (inc y)] 
+        :when (and (not (and (= x xn) (= y yn)))  
+                   (not (contains? visited [xn yn])) 
+                   (contains? nodes [xn yn]))]
+    [xn yn]))
+
+(defn neighbours-fn [nodes visited [[x y] _]]
+  (for [[xn yn] [[(dec x) y] [(inc x) y] [x (dec y)] [x (inc y)]]
+        :when (and (not (contains? visited [xn yn])) 
+                   (contains? nodes [xn yn]))]
+    [xn yn]))
+
+(defn path-weight-fn [nodes upd-node-id curr-node]
+  (get-in nodes [upd-node-id :v]))
+
+(defn calc-min-path [data start-node-id neighbours-fn path-weight-fn]
+  (loop [nodes data visited #{} current-node [start-node-id (get nodes start-node-id)]]
+    (let [nodes (update-adjacent current-node nodes visited neighbours-fn path-weight-fn)
+          current-node (find-shortest-node nodes visited)]
+      (if current-node
+        (recur nodes (conj visited (first current-node)) current-node) 
+        nodes))))
+        
 
 ;620 too high
 (defn run-1 []
-  (let [data (get-data) ]
-    (calc-min-path data)))
+  (let [data (get-data)
+        nodes-with-min-paths (calc-min-path data [0 0] neighbours-fn path-weight-fn)]
+    (get nodes-with-min-paths [99 99])))
+
+(defn test-algo []
+  (let [test-data {0 {:next [1 7]}
+                   1 {:next [0 7 2]}
+                   7 {:next [0 1 8 6]}
+                   2 {:next [1 8 3 5]}
+                   8 {:next [7 2 6]}
+                   6 {:next [8 7 5]}
+                   5 {:next [6 2 3 4]} 
+                   3 {:next [2 5 4]} 
+                   4 {:next [3 5]}} 
+        path-weights {[0 1] 4
+                      [0 7] 8
+                      [1 2] 8
+                      [1 7] 11
+                      [7 6] 1
+                      [7 8] 7
+                      [2 8] 2
+                      [8 6] 6
+                      [6 5] 2
+                      [2 5] 4
+                      [2 3] 7
+                      [3 5] 14
+                      [3 4] 9
+                      [5 4] 10} 
+         neighbours-fn (fn [nodes visited [_ {next-nodes :next} :as cn]]
+                         next-nodes)
+         path-weight-fn (fn [nodes upd-node-id [curr-node-id _]]
+                          (or (get path-weights [upd-node-id curr-node-id])
+                              (get path-weights [curr-node-id upd-node-id])))]
+            
+    (calc-min-path test-data 0 neighbours-fn path-weight-fn))) 
+         
+                  
+
