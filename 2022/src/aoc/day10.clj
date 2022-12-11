5c1b1e1806c13ec2559465b34ec438d57fe2c328
Rolandas G <rol.griskevicius@gmail.com>
2022-12-11 18:12:29 +0200

    day 10 part 2

--- 2022/src/aoc/day10.clj
+++ 2022/src/aoc/day10.clj
@@ -13,12 +13,12 @@
                   (first ds)))
 
 
-(defn get-xs [data clocks]
+(defn get-xs [data acc-fn]
   (loop [data data x 1 clock 1 acc []]
     (if-not data
       acc
       (let [[c op n] (first data)
-            nacc (if (contains? clocks clock) (conj acc [x clock]) acc)]
+            nacc (acc-fn acc clock x)]
         ;(when (contains? clocks clock)
         ;  (println "cc" clock (first data))
         (if (> c 0)
@@ -27,9 +27,26 @@
 
 (defn run-1 [& ds]
   (let [data (get-data (first ds))
-        xd (get-xs data #{20 60 100 140 180 220})]
+        xd (get-xs data (fn [acc clock x]
+                          (if (contains? #{20 60 100 140 180 220} clock) (conj acc [x clock]) acc)))] 
     (reduce (fn [a [b c]] (+ a (* b c))) 0 xd))) 
 
 (comment 
   (run-1))
+
+(defn upd-2 [acc clock x]
+  (let [clock (dec clock)
+        row-pos (quot clock 40)
+        row (get acc row-pos [])
+        col (rem clock 40)
+        pixel-val (if (and (>= col (dec x)) (<= col (inc x))) "#" ".")
+        nrow (assoc row col pixel-val)]
+    (assoc acc row-pos nrow)))
+    
+
+(defn run-2 [& ds]
+  (let [data (get-data (first ds))
+        xd (get-xs data upd-2)] 
+    (doseq [x xd]
+      (println (apply str x)))))
 
