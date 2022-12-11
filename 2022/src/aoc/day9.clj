68541e8fd7c9a88d45cb386a7dbd0d0e0dad9dc9
Rolandas G <rol.griskevicius@gmail.com>
2022-12-11 14:07:03 +0200

    day 9 part 2

--- 2022/src/aoc/day9.clj
+++ 2022/src/aoc/day9.clj
@@ -11,45 +11,85 @@
                                [(-> d s/lower-case keyword) (Integer/parseInt n)])) 
                   (first ds)))
 
-(defn calculate-move-pos [[x y] d]
+(defn new-pos [[x y] d]
   (case d
-        :u [x (dec y)]
-        :d [x (inc y)]
-        :l [(dec x) y]
-        :r [(inc x) y]))
+    :u [x (dec y)]
+    :d [x (inc y)]
+    :l [(dec x) y]
+    :r [(inc x) y]))
 
-(defn normalize-diagonal-move [[x y] d [hx hy]]
-  (case d
-        :u [hx y]
-        :d [hx y]
-        :l [x hy]
-        :r [x hy]))
+(defn iabs [x]
+  (if (< x 0) (* x -1) x))
 
-(defn abs- [a b] (abs (- a b)))
+(defn dist [a b]
+  (iabs (- a b)))
+    
+(defn head-near-tail? [[hx hy] [tx ty]]
+  (and (>= 1 (dist hx tx)) 
+       (>= 1 (dist hy ty))))
 
-(defn move [[[tx ty] hp all-tail-pos] [d n]]
-  (loop [[tx ty :as tp] [tx ty] hp hp atp all-tail-pos n n]
-    ;(println "positions: t h" tp hp)
-    (if (= 0 n)
+(defn move-tail-after-head [[tx ty :as tp] [hx hy :as hp]]
+  (if (head-near-tail? tp hp)
+    tp
+    (let [diff-1-x (if (< hx tx) -1 1)
+          diff-1-y (if (< hy ty) -1 1)]
+
+      (cond (= tx hx)  
+            [tx (+ ty diff-1-y)]
+      
+            (= ty hy) 
+            [(+ tx diff-1-x) ty]
+
+            :else
+            [(+ tx diff-1-x) (+ ty diff-1-y)]))))
+
+(defn move-1 [[tp hp all-tps] [d n]]
+  (loop [tp tp hp hp atp all-tps n n]
+    ;(println "t h" tp hp)
+    (if (= n 0)
       [tp hp atp]
-      (let [[nhx nhy :as nhp] (calculate-move-pos hp d)]
-        (cond (and (< (abs- nhx tx) 2) (< (abs- nhy ty) 2))                    
-              (recur tp nhp atp (dec n))
-
-
-              :else 
-              (let [ntp (calculate-move-pos tp d)
-                    ntp (normalize-diagonal-move ntp d nhp)]
-                (recur ntp nhp (conj atp ntp) (dec n)))))))) 
+      (let [nhp (new-pos hp d)
+            ntp (move-tail-after-head tp nhp)] 
+        (recur ntp nhp (conj atp ntp) (dec n))))))
+          
 
 (defn run-1 [& ds]
   (let [data (get-data (first ds))
-        [_ _ all-tail-pos] (reduce move [[0 0] [0 0] [0 0]] data)]
+        [_ _ all-tail-pos] (reduce move-1 [[0 0] [0 0] [[0 0]]] data)]
+    ;(println "taip pos: " all-tail-pos)
     (count (into #{} all-tail-pos))))
                  
+(def tail-length 9)
+
+
+(defn move-long-tail-once [[tps hp all-tps] d]
+  (doseq [i (range tail-length)]
+    (let [parent-num (dec i)
+          parent (if (< parent-num 0) hp (nth tps parent-num))
+          ntp (move-tail-after-head (nth tps i) parent)]
+      ;(println "moved " i "th from" (nth tps i) "to" ntp "because head is" parent)
+      (assoc! tps i ntp)))
+  [tps (conj all-tps (nth tps (dec tail-length)))])
+        
+(defn move-long-tail [[tps hp all-tps] [d nn]]
+  (loop [tps tps hp hp atp all-tps n nn]
+    (if (= n 0)
+      (do
+        ;(println "move " d nn "->"  [tps hp atp])
+        [tps hp atp])
+      (let [nhp (new-pos hp d)
+            [ntps new-all-tps] (move-long-tail-once [tps nhp atp] d)] 
+        (recur ntps nhp new-all-tps (dec n))))))
+ 
+(defn run-2 [& ds]
+  (let [data (get-data (first ds))
+        [_ _ all-tail-pos] (reduce move-long-tail [(transient (into [] (take tail-length (repeat [0 0])))) [0 0] [[0 0]]] data)]
+    (println "taip pos: " all-tail-pos)
+    (count (into #{} all-tail-pos))))
+ 
 
 (comment
-  ; 5884 too high
-  (run-1 :test))
+  ; 5883
+  (run-1))
 
 
