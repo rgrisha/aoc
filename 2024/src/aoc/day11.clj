a359a119b66d42db710d084580a83b5045235457
Rolandas Griskevicius <rol.griskevicius@gmail.com>
2024-12-11 11:50:38 +0200

    day 11 part 2

--- 2024/src/aoc/day11.clj
+++ 2024/src/aoc/day11.clj
@@ -1,4 +1,4 @@
-(ns aoc.day1
+(ns aoc.day11
   (:require [clojure.string :as s]
             [aoc.utils      :as u]))
 
@@ -6,27 +6,36 @@
   (let [n (mapv identity (str stone))]
     (= 0 (mod (count n) 2))))
 
-;(split-stone 112055)
-(defn split-stone [stone]
+(defn split-stone [stone cnt]
   (let [n (mapv identity (str stone))
         nn (quot (count n) 2)
         l (subvec n 0 nn)
         r (subvec n nn)]
-    [(Long. (apply str l)) (Long. (apply str r))]))
+    [[(Long. (apply str l)) cnt]  [(Long. (apply str r)) cnt]]))
 
-(defn rules [stone]
+(defn rules [[stone cnt]]
   (cond
-    (= 0 stone) 1
-    (even-digits? stone)  (split-stone stone)
-    :else (* stone 2024)))
+    (= 0 stone) [[1 cnt]]
+    (even-digits? stone)  (split-stone stone cnt)
+    :else [[(* stone 2024) cnt]]))
+
+(defn compress [stones]
+  (->> stones
+       (group-by first)
+       vals
+       (mapv (fn [l] [(ffirst l) (apply + (map second l))]))))
 
 (defn blink [stones cnt]
-  (loop [stones stones cnt cnt]
+  (loop [stones (map (fn [st] [st 1]) stones) cnt cnt]
     (if (> cnt 0)
-       (recur (flatten (map rules stones)) (dec cnt))
-       stones)))
+       (recur (compress (mapcat rules stones)) (dec cnt))
+       (apply + (map second stones)))))
+
 
 (defn part-1 []
-  (count (blink [0 89741 316108 7641 756 9 7832357 91] 25)))
+  (blink [0 89741 316108 7641 756 9 7832357 91] 25))
 
-(part-1)
+(defn part-2 []
+  (blink [0 89741 316108 7641 756 9 7832357 91] 75))
+
+
