b5cfa0577e1db4ba261941872ebe8e3c6c1f5ab6
Rolandas Griskevicius <rol.griskevicius@gmail.com>
2025-12-05 12:56:52 +0200

    day 5 done

--- 2025/src/aoc/day5.clj
+++ 2025/src/aoc/day5.clj
@@ -4,7 +4,50 @@
             [clojure.set :as cs]))
 
 (defn line-fn [l]
-  (map-indexed (fn [i e] [i e]) (seq l)))
+  l)
 
 (defn get-data [t]
-  (let [lines (u/get-day-data 4 line-fn t)
+  (let [lines (u/get-day-data 5 line-fn t)]
+    (loop [ranges [] ids [] lines lines]
+      (if (nil? lines)
+        [ranges ids]
+        (let [[f s] (s/split (first lines) #"-")]
+          (cond
+            (or (nil? f) (.isBlank f))
+            (recur ranges ids (next lines))
+
+            (nil? s)
+            (recur ranges (conj ids (Long/parseLong f)) (next lines))
+
+            :else
+            (recur (conj ranges [(Long/parseLong f) (Long/parseLong s)]) ids (next lines))))))))
+
+(defn part-1 []
+  (let [[ranges ids] (get-data nil)]
+    (->> ids
+         (keep (fn [e] (some (fn [[f s]] (and (>= e f) (<= e s))) ranges)))
+         count)))
+
+(defn descartes [ranges]
+  (for [a ranges
+        b ranges
+        :when (not= a b)]
+    [a b]))
+
+(defn merge-all-ranges [ranges]
+  (let [red-fn (fn [[a [cel cer]] [el er]]
+                 (if (< cer el)
+                   [(conj a [cel cer]) [el er]]
+                   [a [cel (max cer er)]]))
+        ranges (sort-by first ranges)
+        [a l] (reduce red-fn [[] (first ranges)] (next ranges))]
+    (conj a l)))
+
+
+; 355555479253787
+(defn part-2 []
+  (let [ranges (first (get-data nil))]
+     (->> ranges
+          merge-all-ranges
+          (map (fn [[a b]] (inc (- b a))))
+          (apply +))))
